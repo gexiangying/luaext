8a599dc03678a0ac5c5450735d6f121557a5a6dc
U-PC2011111609GTE\Administrator <Administrator@PC2011111609GTE.(none)>
2012-05-21 11:06:09 +0800

    add test param

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 #include "include/lauxlib.h"
 static int test(lua_State *L)
 {
-	MessageBox(NULL,"hello","world",MB_OK);
+const char* title = luaL_checkstring(L,1);
+const char* content = luaL_checkstring(L,2);
+	MessageBox(NULL,content,title,MB_OK);
 	return 0;
 }
-static const struct luaL_Reg mylib [] = {
+static const struct luaL_Reg luaext [] = {
 	{"test",test},
 	{NULL,NULL}
 };
-int luaopen_mylib(lua_State *L){
-	luaL_register(L,"mylib",mylib);
+int luaopen_luaext(lua_State *L){
+	luaL_register(L,"luaext",luaext);
 	return 1;
 };
 
