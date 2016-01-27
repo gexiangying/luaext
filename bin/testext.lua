d66363c59cb1fd4afce9793de3010dda605b2939
gexiangying <19727709@qq.com>
2016-01-27 21:38:04 +0700

    change testext.lua

--- bin/testext.lua
+++ bin/testext.lua
@@ -1,19 +1,24 @@
 local luaext = require "luaext"
+--[[
 a = luaext.encrypt("gexy")
 print(a)
 print(luaext.decrypt(a))
 print(luaext.md5("gexy"))
 local id = luaext.guid()
 print(id)
-
+--]]
 local luaext_pipe = require "luaext.pipe"
-local pipe = luaext_pipe.new("lua -v")
+local pipe = luaext_pipe.new("ipconfig /all")
+local macs = {}
 repeat
 	local line = pipe:getline()
 	if line then
-		print(line)
+		local mac = string.match(line,"%s+(%x%x%-%x%x%-%x%x%-%x%x%-%x%x%-%x%x)%s+")
+		if mac then macs[mac] = true end
 	end
 until not line
-assert(pipe)
+for k,v in pairs(macs) do
+	print(k)
+end
 pipe:closeout()
 pipe:closein()
